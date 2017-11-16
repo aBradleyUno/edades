6729aca83ed9d3972d8198f2feafd43bdbfd9a01
Alexander Bradley <alexandro20.0@icloud.com>
2017-11-16 12:01:55 -0600

    Corrige errores en Edad.py

--- Edad.py
+++ Edad.py
@@ -3,19 +3,19 @@ class Edad():
         self.resultado = ''
 
     def calculate_edad(self,num1):
-		if type(num1) is str:
-			self.resultado= "no valido"
-		elif num1<0:
-			self.resultado = "no existes"
-		elif num1<13 and num1>=0:
-			self.resultado = "eres nino"
-		elif num1>=13 and num1<18:
-			self.resultado = "eres adolecente"
-		elif num1>=18 and num1<65:
-			self.resultado = "eres adulto"
-		elif num1>=65 and num1<120:
-			self.resultado = "eres adulto mayor"
-		else:
-			self.resultado = "eres mumm-ra"
+        if type(num1) is str:
+            self.resultado= "no valido"
+        elif num1<0:
+            self.resultado = "no existes"
+        elif num1<13 and num1>=0:
+            self.resultado = "eres nino"
+        elif num1>=13 and num1<18:
+            self.resultado = "eres adolecente"
+        elif num1>=18 and num1<65:
+            self.resultado = "eres adulto"
+        elif num1>=65 and num1<120:
+            self.resultado = "eres adulto mayor"
+        else:
+            self.resultado = "eres mumm-ra"
     def getResultado(self):
         return self.resultado
